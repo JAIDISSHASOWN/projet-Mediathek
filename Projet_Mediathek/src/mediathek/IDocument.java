15ad57e4dee594c30dff56313edbfdebda5088f8
nathan <nathanmaciellavergne@gmail.com>
2025-03-29 17:42:37 +0100

    Brainstorming

--- Projet_Mediathek/src/mediathek/IDocument.java
+++ Projet_Mediathek/src/mediathek/IDocument.java
@@ -1,6 +1,19 @@
 package mediathek;
+import Exception.ReservationException;
+import Exception.EmpruntException;
 
 public interface IDocument {
-    int getId();
-    String getTitre();
+
+    public interface Document {
+        int numero();
+        // exception si déjà réservé ou emprunté
+        void reserver (Abonne ab) throws ReservationException;
+        // exception si réservé pour une autre abonné ou déjà emprunté
+        void emprunter(Abonne ab) throws EmpruntException;
+        // sert au retour d’un document ou à l’annulation d‘une réservation
+        void retourner();
+    }
+
+//    int getId();
+//    String getTitre();
 }
