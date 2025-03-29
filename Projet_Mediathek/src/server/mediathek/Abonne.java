0512c1c2712adef1f5341c7c2dac96787739213c
nathan <nathanmaciellavergne@gmail.com>
2025-03-29 20:18:33 +0100

    Age d'un Abonne

--- Projet_Mediathek/src/server/mediathek/Abonne.java
+++ Projet_Mediathek/src/server/mediathek/Abonne.java
@@ -1,11 +1,26 @@
 package server.mediathek;
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
+import java.util.Date;
+
+
 public class Abonne {
     private int numero;
     private String nom;
+    private Date dateAnniversaire;
 
-    public Abonne (int numero, String nom){
+    public Abonne (int numero, String nomDate, Date dateAnniversaire){
         this.numero = numero;
         this.nom = nom;
+        this.dateAnniversaire = dateAnniversaire;
+    }
+
+    public long getAge() {
+        return Period.between(
+                dateAnniversaire.toInstant().atZone(ZoneId.systemDefault()).toLocalDate(),
+                LocalDate.now()
+        ).getYears();
     }
 }
