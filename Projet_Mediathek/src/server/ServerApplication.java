8cb10ad6b0cddfac46640805c0db5b9e8072e81b
Adam <graindorgeadam2@gmail.com>
2025-03-30 04:48:34 +0200

    qui a oublié laccolade frr jv le tuer

--- Projet_Mediathek/src/server/ServerApplication.java
+++ Projet_Mediathek/src/server/ServerApplication.java
@@ -1,15 +1,12 @@
 package server;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 import com.unodos.Server;
 import server.service.ServiceEmprunt;
 import server.service.ServiceReservation;
 import server.service.ServiceRetour;
 
-import java.io.IOException;
 public class ServerApplication {
 
     private final static int RESERVATION_PORT = 2000;
@@ -17,16 +14,18 @@ public class ServerApplication {
     private final static int RETOUR_PORT = 4000;
 
     public static void main(String[] args) {
-            try {
-                new Thread(new Server(ServiceReservation.class, RESERVATION_PORT)).start();
-                System.out.println("Serveur de réservation lancé sur le port " + RESERVATION_PORT);
-                new Thread(new Server(ServiceEmprunt.class, EMPRUNT_PORT)).start();
-                System.out.println("Serveur d'emprunt lancé sur le port " + EMPRUNT_PORT);
-                new Thread(new Server(ServiceRetour.class, RETOUR_PORT)).start();
-                System.out.println("Server de retour lancé sur le port " + RETOUR_PORT);
-            } catch (IOException e) {
-                System.err.println("Erreur lors de la céation du serveur : " + e.getMessage());
-            }
+        try {
+            new Thread(new Server(ServiceReservation.class, RESERVATION_PORT)).start();
+            System.out.println("Serveur de réservation lancé sur le port " + RESERVATION_PORT);
+
+            new Thread(new Server(ServiceEmprunt.class, EMPRUNT_PORT)).start();
+            System.out.println("Serveur d'emprunt lancé sur le port " + EMPRUNT_PORT);
+
+            new Thread(new Server(ServiceRetour.class, RETOUR_PORT)).start();
+            System.out.println("Serveur de retour lancé sur le port " + RETOUR_PORT);
+
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la création du serveur : " + e.getMessage());
         }
     }
 }
